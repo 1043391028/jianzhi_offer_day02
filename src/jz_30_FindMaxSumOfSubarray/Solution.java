bb7aa2f491644a4bbe83e17b4d0aa249182988b8
gwzstar <guanweizheng66@163.com>
2020-09-19 23:22:02 +0800

    剑指offer36题

--- src/jz_30_FindMaxSumOfSubarray/Solution.java
+++ src/jz_30_FindMaxSumOfSubarray/Solution.java
@@ -3,7 +3,7 @@ package jz_30_FindMaxSumOfSubarray;
 public class Solution {
     public int FindGreatestSumOfSubArray(int[] array) {
         int max = array[0];
-        for (int i = 1; i < array.length; i++) {
+        for (int i = 0; i < array.length; i++) {
             array[i] += array[i - 1] > 0 ? array[i - 1] : 0;
             max = Math.max(max, array[i]);
         }
